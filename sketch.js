72d20d13cccc6cbad0a604a4d133b6736db04e7b
dheerbansal <67144062+dheerbansal@users.noreply.github.com>
2020-08-05 15:49:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,16 +6,16 @@ var d;
 function setup() {
   createCanvas(1200,400);
   fixedRect = createSprite(1000,190, 50, 80);
-  fixedRect.shapeColor = "grey";
-  //fixedRect.debug = true;
-  movingRect = createSprite(35, 190,35,35);
-  movingRect.shapeColor = "white";
-  //movingRect.debug = false;
-  speed  = random(55,90);
+  fixedRect.shapeColor = "black";
+  fixedRect.debug = true;
+  movingRect = createSprite(35, 190,50,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = false;
+  speed  = random(55,190);
   weight = random(400,1500);
 
   movingRect.velocityX = speed;
- // console.log(movingRect.velocityX);
+  console.log(movingRect.velocityX);
 
   
   
@@ -32,26 +32,25 @@ function draw() {
  } 
 
  
- d = (speed * weight *speed*0.5)/22500
 
-if(d < 100){
+if(d <= 80){
   movingRect.shapeColor = "green";
-  
+  fixedRect.shapeColor = "green";
 }
 
-if(d >= 100 && d < 180){
+if(d >= 81 && d <= 180){
   movingRect.shapeColor = "yellow";
-  
+  fixedRect.shapeColor = "yellow";
 
 }
 
 if(d > 180){
   movingRect.shapeColor = "red";
-  
+  fixedRect.shapeColor = "red";
 
 }
 
-
+d = (speed * weight *speed*0.5)/22500
 
 
 
